6545a6fdbb4fe559c76131ec18be681b74f672e6
wujc <15755081023@163.com>
2020-11-10 16:36:54 +0800

    feat: 修改sort 代码

--- src/sort/quickSort.js
+++ src/sort/quickSort.js
@@ -48,4 +48,65 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
 }
 
 
-console.log(quickSort([1, 1, 5, 3, 4, 7, 2, 3]));
+function quickSort1(arr) {
+  // 交换
+  function swap(arr, a, b) {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  // 分区
+  function partition(arr, left, right) {
+    /**
+       * 开始时不知最终pivot的存放位置，可以先将pivot交换到后面去
+       * 这里直接定义最右边的元素为基准
+       */
+    const pivot = arr[right];
+    /**
+       * 存放小于pivot的元素时，是紧挨着上一元素的，否则空隙里存放的可能是大于pivot的元素，
+       * 故声明一个storeIndex变量，并初始化为left来依次紧挨着存放小于pivot的元素。
+       */
+    let storeIndex = left;
+    for (let i = left; i < right; i++) {
+      if (arr[i] < pivot) {
+        /**
+               * 遍历数组，找到小于的pivot的元素，（大于pivot的元素会跳过）
+               * 将循环i次时得到的元素，通过swap交换放到storeIndex处，
+               * 并对storeIndex递增1，表示下一个可能要交换的位置
+               */
+        swap(arr, storeIndex, i);
+        storeIndex++;
+      }
+    }
+    // 最后： 将pivot交换到storeIndex处，基准元素放置到最终正确位置上
+    swap(arr, right, storeIndex);
+    return storeIndex;
+  }
+
+  function sort(arr, left, right) {
+    if (left > right) return;
+
+    const storeIndex = partition(arr, left, right);
+    sort(arr, left, storeIndex - 1);
+    sort(arr, storeIndex + 1, right);
+  }
+
+  sort(arr, 0, arr.length - 1);
+  return arr;
+}
+
+
+
+const aa = (new Array(10000)).fill(0).map(item => { return parseInt(Math.random() * 20000); });
+
+
+const bb = [...aa];
+const cc = [...aa];
+console.time('sort');
+quickSort(bb);
+console.timeEnd('sort');
+
+console.time('sort1');
+quickSort1(cc);
+console.timeEnd('sort1');
